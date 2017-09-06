32f9beec5aaf35f0f5d97d79e6c5ce4a130f6bba
danizavtz <daniel.pires@splab.ufcg.edu.br>
2017-09-06 14:07:49 -0300

    Inserção de url funcional,para get de trabalhador

--- server/services/trabalhador.service.js
+++ server/services/trabalhador.service.js
@@ -1,7 +1,17 @@
 (function() {
-    exports.getTrabalhadores = function(req, res){
-    	res.json('get');
-    	res.end();
-    }
+  'use strict';
+  var postgres = require('../../lib/postgres');
+
+  exports.getTrabalhadores = function(req, res) {
+    var sql = 'SELECT t.* FROM trabalhador t';
+    postgres.client.query(sql, function(err, results) {
+    	if(err){
+    		console.log(err);
+    		return res.json(err.detail);
+    	}
+      res.json(results.rows);
+      res.end();
+    });
+  }
 
 }());
